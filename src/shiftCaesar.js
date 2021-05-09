d58d81b171c3684173c07e687ee5a3145c6ee348
Andrewmurashko <anamay94.am@gmail.com>
2021-05-09 22:15:22 +0300

    shift caesar3

--- src/shiftCaesar.js
+++ src/shiftCaesar.js
@@ -9,7 +9,9 @@ const shiftCaesar = (text, shift) => {
       const code = text.charCodeAt(i);
       if (code >= 65 && code <= 90) {
         letter = String.fromCharCode(((code - 65 + shift) % 26) + 65);
-      } 
+      } else if (code >= 97 && code <= 122) {
+        letter = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }
     }
     resault += letter;
   }
